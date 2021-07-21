35ee6e27d6407864fd035ca659404eb050011630
trqii <3038605486@qq.com>
2021-07-21 09:51:34 +0800

    8_4

---
+++ The eighth chapter/programming_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define SIZE  (int)(sizeof (a) / sizeof (a[0])) 
+#define N 10
+
+int main(void)
+{
+	int a[N], i;
+
+	printf("Enter %d numbers: ", N);
+	for (i = 0; i < SIZE; i++)
+		scanf("%d", &a[i]);
+
+	printf("In reverse order: ");
+	for (i = SIZE - 1; i >= 0; i--)
+		printf(" %d", a[i]);
+	printf("\n");
+
+	return 0;
+}
