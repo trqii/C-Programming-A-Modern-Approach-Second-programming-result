40600d220ba8b4aefa3185866cab374b43ead5c9
trqii <3038605486@qq.com>
2021-08-23 13:09:47 +0800

    13_5

---
+++ The thirteenth chapter/programming_5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	for (int i = argc - 1; i >= 1; i--) 
+	{
+		sum += atoi(argv[i]);
+	}
+
+	printf("Total: %d", sum);
+
+	return 0;
+}
